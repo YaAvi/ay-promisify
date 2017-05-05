5b667e2cced004d8df2c4b70c2b27734dc25a14c
Avi Y <aviyaakov1@gmail.com>
2017-05-05 11:53:07 +0300

    two liner

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-module.exports = fn => (...args) => new Promise((resolve, reject) => {
-	args.push((err, data) => err ? reject(err) : resolve(data));
-	fn.apply(fn, args);
-})
+module.exports = fn => (...args) => new Promise((resolve, reject) =>
+	fn(...args, (err, data) => err ? reject(err) : resolve(data)));
